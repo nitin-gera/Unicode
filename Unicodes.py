a56896f573b0a07106c12a2938275590dc9b05f6
nitin-gera <68019550+nitin-gera@users.noreply.github.com>
2020-11-16 14:48:27 +0530

    Use of Unicode in Python
    This example demonstrates how to use unicode in Python

---
+++ Unicodes.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 29 14:43:28 2018
+
+@author: nitingera
+"""
+
+euro = u"\N{euro sign}"
+dollar = u"\N{dollar sign}"
+yen = u"\N{yen sign}"
+
+print(euro)
+print(dollar)
+print(yen)
+
+print("He said \"No you don't!\" again."
+      'What\'s up with him?')
+
+print('''This string has three lines in it, with a 'quote',
+another "quote", and with just one embedded newline \
+since we have escaped one of them.''')
